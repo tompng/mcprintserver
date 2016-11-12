9066f7a7bb3d16f4bd230f5e914a9e56d114c6d1
tompng <tomoyapenguin@gmail.com>
2016-11-13 01:47:11 +0900

    face4

--- obj_extract/block_textures.rb
+++ obj_extract/block_textures.rb
@@ -1,6 +1,96 @@
 require_relative '../mc_world/world'
 require 'json'
 module BlockTextures
+  class CubeBlock
+    def initialize top, side=nil, bottom=nil
+      @top = top
+      @side = side || top
+      @bottom = bottom || top
+    end
+    def cube?
+      true
+    end
+    def face dx, dy, dz
+      if dy>0
+        BlockTextures.texture_uvs @top
+      elsif dy<0
+        BlockTextures.texture_uvs @bottom
+      else
+        BlockTextures.texture_uvs @side
+      end
+    end
+  end
+  def self.gen_mtl
+    grass_color = [0x60, 0x90, 0x30]
+    stem_color1 = [0x40, 0x80, 0x10]
+    stem_color2 = [0x80, 0x70, 0x10]
+    leaf_color  = [0x40, 0x80, 0x10]
+    redstone_color = [0x80,-0xff,-0xff]
+    colors = {
+      'grass_top' => grass_color,
+      'fern' => grass_color,
+      'tallgrass' => grass_color,
+      'double_plant_fern_bottom' => grass_color,
+      'double_plant_fern_top' => grass_color,
+      'double_plant_grass_bottom' => grass_color,
+      'double_plant_grass_top' => grass_color,
+      'leaves_acacia' => leaf_color,
+      'leaves_big_oak' => leaf_color,
+      'leaves_birch' => [0x60,0x80,0x40],
+      'leaves_jungle' => leaf_color,
+      'leaves_oak' => leaf_color,
+      'leaves_spruce' => [0x40,0x80,0x40],
+      'melon_stem_connected' => stem_color1,
+      'melon_stem_disconnected' => stem_color1,
+      'pumpkin_stem_connected' => stem_color2,
+      'pumpkin_stem_disconnected' => stem_color2,
+      'vine' => grass_color,
+      'waterlily' => grass_color,
+      'redstone_dust_dot' => redstone_color,
+      'redstone_dust_line0' => redstone_color,
+      'redstone_dust_line1' => redstone_color,
+    }
+
+    png_dir = "#{File.dirname(__FILE__)}/blocks"
+    files = Dir.glob("#{png_dir}/*.png")
+    image = ChunkyPNG::Image.new 512, 512
+    meta = {}
+    files.each_slice(32).with_index do |slice, y|
+      slice.each_with_index do |file, x|
+        name = File.basename(file).gsub(/\.png$/, '')
+        meta[name] = {x: x, y: y}
+        img = ChunkyPNG::Image.from_file file
+        color = colors[name]
+        (0...16).to_a.repeated_permutation 2 do |ix, iy|
+          c = img[ix%img.width, iy%img.height]
+          if color
+            a,b,g,r = 4.times.map{|i|(c>>(8*i))&0xff}
+            r += color[0]-0x80
+            g += color[1]-0x80
+            b += color[2]-0x80
+            a = 0xff if name =~ /leaves/
+            r,g,b = [r,g,b].map{|c|[0,c,0xff].sort[1]}
+            image[16*x+ix, 16*y+iy] = [a,b,g,r].each_with_index.map{|c,i|c<<(8*i)}.inject(:|)
+          else
+            image[16*x+ix, 16*y+iy] = c
+          end
+        end
+      end
+    end
+    @meta = meta
+    File.write 'meta.json', meta.to_json
+    image.save 'texture.png'
+    File.write 'block.mtl', %(newmtl block\n  map_Ka texture.png\n  map_Kd texture.png\n)
+  end
+  gen_mtl
+
+  def self.texture_uvs texture_id
+    pos = @meta[texture_id.to_s]
+    [[0,0],[1,0],[1,1],[0,1]].map do |x, y|
+      [(pos[:x]+x)/32.0, (32-pos[:y]-y)/32.0]
+    end
+  end
+
   def self.estimateds
     files = JSON.parse File.read('meta.json')
     MCWorld::Block::BlockDefinition.keys.map{|key|
@@ -10,23 +100,23 @@ module BlockTextures
       }.reject(&:empty?)
       keys = matcheds.inject(:&)
       if keys&.size == 1
-        [MCWorld::Block.const_get(key), keys.first]
+        [MCWorld::Block.const_get(key), CubeBlock.new(keys.first)]
       end
     }.compact.to_h
   end
   def self.overrides
     {
-      MCWorld::Block::Grass => 'grass_top',
-      MCWorld::Block::Dirt => 'dirt',
-      MCWorld::Block::Stone => 'stone',
-      MCWorld::Block::SlimeBlock => 'slime',
-      MCWorld::Block::EndStoneBricks => 'end_bricks',
-      MCWorld::Block::Andesite => 'stone_andesite',
-      MCWorld::Block::Granite => 'stone_granite',
-      MCWorld::Block::Diorite => 'stone_diorite',
-      MCWorld::Block::PolishedAndesite => 'stone_andesite_smooth',
-      MCWorld::Block::PolishedGranite => 'stone_granite_smooth',
-      MCWorld::Block::PolishedDiorite => 'stone_diorite_smooth'
+      MCWorld::Block::Grass => CubeBlock.new('grass_top', 'grass_side', 'dirt'),
+      MCWorld::Block::Dirt => CubeBlock.new('dirt'),
+      MCWorld::Block::Stone => CubeBlock.new('stone'),
+      MCWorld::Block::SlimeBlock => CubeBlock.new('slime'),
+      MCWorld::Block::EndStoneBricks => CubeBlock.new('end_bricks'),
+      MCWorld::Block::Andesite => CubeBlock.new('stone_andesite'),
+      MCWorld::Block::Granite => CubeBlock.new('stone_granite'),
+      MCWorld::Block::Diorite => CubeBlock.new('stone_diorite'),
+      MCWorld::Block::PolishedAndesite => CubeBlock.new('stone_andesite_smooth'),
+      MCWorld::Block::PolishedGranite => CubeBlock.new('stone_granite_smooth'),
+      MCWorld::Block::PolishedDiorite => CubeBlock.new('stone_diorite_smooth')
     }
   end
   Info = estimateds.merge overrides
--- obj_extract/obj_extract.rb
+++ obj_extract/obj_extract.rb
@@ -3,77 +3,6 @@ require 'chunky_png'
 require 'json'
 require_relative './block_textures'
 class OBJExtract
-  def self.gen_mtl
-    grass_color = [0x60, 0x90, 0x30]
-    stem_color1 = [0x40, 0x80, 0x10]
-    stem_color2 = [0x80, 0x70, 0x10]
-    leaf_color  = [0x40, 0x80, 0x10]
-    redstone_color = [0x80,-0xff,-0xff]
-    colors = {
-      'grass_top' => grass_color,
-      'fern' => grass_color,
-      'tallgrass' => grass_color,
-      'double_plant_fern_bottom' => grass_color,
-      'double_plant_fern_top' => grass_color,
-      'double_plant_grass_bottom' => grass_color,
-      'double_plant_grass_top' => grass_color,
-      'leaves_acacia' => leaf_color,
-      'leaves_big_oak' => leaf_color,
-      'leaves_birch' => [0x60,0x80,0x40],
-      'leaves_jungle' => leaf_color,
-      'leaves_oak' => leaf_color,
-      'leaves_spruce' => [0x40,0x80,0x40],
-      'melon_stem_connected' => stem_color1,
-      'melon_stem_disconnected' => stem_color1,
-      'pumpkin_stem_connected' => stem_color2,
-      'pumpkin_stem_disconnected' => stem_color2,
-      'vine' => grass_color,
-      'waterlily' => grass_color,
-      'redstone_dust_dot' => redstone_color,
-      'redstone_dust_line0' => redstone_color,
-      'redstone_dust_line1' => redstone_color,
-    }
-
-    png_dir = "#{File.dirname(__FILE__)}/blocks"
-    files = Dir.glob("#{png_dir}/*.png")
-    image = ChunkyPNG::Image.new 512, 512
-    meta = {}
-    files.each_slice(32).with_index do |slice, y|
-      slice.each_with_index do |file, x|
-        name = File.basename(file).gsub(/\.png$/, '')
-        meta[name] = {x: x, y: y}
-        img = ChunkyPNG::Image.from_file file
-        color = colors[name]
-        (0...16).to_a.repeated_permutation 2 do |ix, iy|
-          c = img[ix%img.width, iy%img.height]
-          if color
-            a,b,g,r = 4.times.map{|i|(c>>(8*i))&0xff}
-            r += color[0]-0x80
-            g += color[1]-0x80
-            b += color[2]-0x80
-            a = 0xff if name =~ /leaves/
-            r,g,b = [r,g,b].map{|c|[0,c,0xff].sort[1]}
-            image[16*x+ix, 16*y+iy] = [a,b,g,r].each_with_index.map{|c,i|c<<(8*i)}.inject(:|)
-          else
-            image[16*x+ix, 16*y+iy] = c
-          end
-        end
-      end
-    end
-    @meta = meta
-    File.write 'meta.json', meta.to_json
-    image.save 'texture.png'
-    File.write 'block.mtl', %(newmtl block\n  map_Ka texture.png\n  map_Kd texture.png\n)
-  end
-  gen_mtl
-
-  def self.texture_uvs texture_id
-    pos = @meta[texture_id.to_s]
-    [[0,0],[1,0],[1,1],[0,1]].map do |x, y|
-      [(pos[:x]+x)/32.0, (32-pos[:y]-y)/32.0]
-    end
-  end
-
   def initialize file
     @world = MCWorld::World.new file: file
     @verts = []
@@ -92,7 +21,12 @@ class OBJExtract
     @uvs.size
   end
 
-  def face a, b, c
+  def face a, b, c, d=nil
+    if d
+      face a,b,c
+      face a,c,d
+      return
+    end
     pa, pb, pc = @verts[a[0]-1], @verts[b[0]-1], @verts[c[0]-1]
     ax, ay, az = pa.zip(pc).map{|a,c|a-c}
     bx, by, bz = pb.zip(pc).map{|b,c|b-c}
@@ -105,7 +39,7 @@ class OBJExtract
     @faces << [[*a,n],[*b,n],[*c,n]]
   end
 
-  def quadface pos, dir, texture_id
+  def quadface pos, dir, texture_uvs
     val, axis = dir.each.with_index.find{ |val, axis| val.nonzero? }
     p00, p01, p10, p11 = 4.times.map { dir.dup }
     p00[ (axis+1) % 3] = -1
@@ -120,10 +54,9 @@ class OBJExtract
     vertex = ->(p){pos.zip(p).map{|ps,p|ps+(1+p)/2}}
 
     verts = [vertex[p00], vertex[p10], vertex[p11], vertex[p01]].map{|v|vert *v}
-    uvs = self.class.texture_uvs(texture_id).map{|tc|uv *tc}
+    uvs = texture_uvs.map{|tc|uv *tc}
 
-    face *[0,1,2].map{|i|[verts[i],uvs[i]]}
-    face *[0,2,3].map{|i|[verts[i],uvs[i]]}
+    face *verts.zip(uvs)
   end
 
   def data
@@ -145,10 +78,16 @@ class OBJExtract
         (zmin..zmax).each{|z|
           block = BlockTextures::Info[get[x,y,z]]
           next unless block
-          [[-1,0,0],[1,0,0],[0,-1,0],[0,1,0],[0,0,-1],[0,0,1]].each{|dx,dy,dz|
-            facing = BlockTextures::Info[get[x+dx,y+dy,z+dz]]
-            quadface [x-(xmin+xmax)/2,y-(ymin+ymax)/2,z-(zmin+zmax)/2], [dx,dy,dz], block unless facing
-          }
+          pos = [x-(xmin+xmax)/2,y-(ymin+ymax)/2,z-(zmin+zmax)/2]
+          if block.cube?
+            [[-1,0,0],[1,0,0],[0,-1,0],[0,1,0],[0,0,-1],[0,0,1]].each{|dx,dy,dz|
+              facing = BlockTextures::Info[get[x+dx,y+dy,z+dz]]
+              next if facing&.cube?
+              quadface pos, [dx,dy,dz], block.face(dx,dy,dz)
+            }
+          else
+            block.render self, pos
+          end
         }
       }
     }
