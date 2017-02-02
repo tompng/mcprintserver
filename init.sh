85a2e262935f45b20b8687eb7d476ae8e837a167
tompng <tomoyapenguin@gmail.com>
2017-02-03 03:39:40 +0900

    update init.sh

--- init.sh
+++ init.sh
@@ -6,9 +6,28 @@
 export HOME=/home/ec2-user
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/mcprintserver
-test $1 = "start" && nohup ruby server.rb > /dev/null &
+start(){
+  if pgrep -f "ruby server.rb"
+  then
+    'already started'
+  else
+    nohup ruby server.rb > /dev/null &
+  fi
+}
+stop(){
+  if pgrep -f ruby.server.rb
+  then
+    kill -INT `pgrep -f "ruby server.rb"`
+    kill -INT `pgrep -f java`
+    sleep 2
+    kill -KILL `pgrep -f "ruby server.rb"`
+    kill -KILL `pgrep -f java`
+  fi
+}
+test $1 = "start" && start
+test $1 = "stop" && stop
 
 # sudo cp init.sh /etc/init.d/mcprintserver
 # sudo chmod +x /etc/init.d/mcprintserver
-# chkconfig --add mcprintserver
+# sudo chkconfig --add mcprintserver
 # service mcprintserver start
