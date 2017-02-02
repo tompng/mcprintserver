76036d0a2b49c10bae1d6b0e9d0e3df61afeba32
tompng <tomoyapenguin@gmail.com>
2017-02-02 19:58:35 +0900

    frame&json gen

--- generate_world.rb
+++ generate_world.rb
@@ -153,84 +153,84 @@ gen_tree = ->x,z,y,oak{
     end
   end
 }
-File.write "spigot/world/region/r.0.0.mca", world.encode
-#
-#
-# chunks = JSON.parse File.read('chunks.json') rescue nil
-# chunks ||= [[1,1],[1,7],[1,9],[1,11],[1,13],[3,11],[4,6],[4,25],[4,27],[4,30],[6,1],[6,20],[6,24],[6,26],[6,28],[6,30],[7,8],[7,10],[7,22],[8,1],[8,26],[8,28],[8,30],[9,10],[10,1],[10,27],[11,18],[11,20],[11,25],[12,5],[12,12],[13,3],[14,5],[14,14],[14,21],[14,29],[15,18],[15,25],[16,1],[16,15],[16,23],[16,28],[17,4],[17,19],[17,26],[18,6],[18,8],[18,29],[19,22],[19,24],[20,26],[20,30],[21,9],[21,17],[22,6],[22,20],[22,30],[23,8],[24,19],[24,21],[25,8],[25,24],[26,1],[26,6],[26,18],[26,20],[26,22],[27,14],[27,27],[28,4],[28,12],[28,21],[29,1],[29,15],[29,19],[29,23],[30,10],[30,17],[30,21],[30,26],[30,28],[30,30]]
-# jsonfile = 'areas.json'
-# world = MCWorld::World.new file: infile
-# ymax = ->cx,cz{
-#   world.chunk(cx,cz).instance_eval{@blocks}.size
-# }
-# void_block = MCWorld::Block[217]
-# block_ids = Set.new %w(Bedrock Stone Sandstone Grass Dirt Gravel Sand Sandstone IronOre CoalOre Cobblestone).map{|name|
-#   MCWorld::Block.const_get(name).id
-# }
-# bottom = ->x,z{
-#   ymax[x/16,z/16].times.reverse_each.find do |y|
-#     block_ids.include? world[x,z,y]&.id
-#   end
-# }
-# size = 16
-# offset = 8
-# ccoords = (8-size/2...8+size/2).to_a.repeated_permutation 2
-# acoords = (8-size/2-offset...8+size/2+offset).to_a.repeated_permutation 2
-# areas = []
-# chunks.each do |cx,cz|
-#   cpos = ccoords.map{|x,z|[cx*16+x,cz*16+z]}
-#   apos = acoords.map{|x,z|[cx*16+x,cz*16+z]}
-#   cmin = cpos.map{|x,z|bottom[x,z]}.min
-#   amin = apos.map{|x,z|bottom[x,z]}.min
-#   (8-size/2-1..8+size/2).to_a.repeated_permutation(2).each do |x,z|
-#     (amin...cmin).each{|y|world[16*cx+x,16*cz+z,y]=MCWorld::Block::Bedrock}
-#   end
-#   ox, oz, oy = 16*cx+8-size/2, 16*cz+8-size/2, cmin
-#   areas << {
-#     print: {
-#       min: {x: ox, z: oz, y: cmin},
-#       max: {x: ox+size-1, z: oz+size-1, y: cmin+size-1}
-#     },
-#     area: {
-#       min: {x: ox-offset+1, z: oz-offset+1, y: amin},
-#       max: {x: ox+size+offset-2, z: oz+size+offset-2, y: cmin+size+offset-2}
-#     }
-#   }
-#   (-1..size).each{|i|
-#     world[ox+i, oz-1, cmin-1]=MCWorld::Block::Bedrock
-#     world[ox+i, oz+size, cmin-1]=MCWorld::Block::Bedrock
-#     world[ox-1, oz+i, cmin-1]=MCWorld::Block::Bedrock
-#     world[ox+size, oz+i, cmin-1]=MCWorld::Block::Bedrock
-#
-#     world[ox+i, oz-1, cmin+size]=MCWorld::Block::Bedrock
-#     world[ox+i, oz+size, cmin+size]=MCWorld::Block::Bedrock
-#     world[ox-1, oz+i, cmin+size]=MCWorld::Block::Bedrock
-#     world[ox+size, oz+i, cmin+size]=MCWorld::Block::Bedrock
-#     world[ox-1,oz-1,cmin+i]=MCWorld::Block::Bedrock
-#     world[ox-1,oz+size,cmin+i]=MCWorld::Block::Bedrock
-#     world[ox+size,oz-1,cmin+i]=MCWorld::Block::Bedrock
-#     world[ox+size,oz+size,cmin+i]=MCWorld::Block::Bedrock
-#   }
-#   apos.each do |x,z|
-#     world[x,z,amin-1]=MCWorld::Block::Bedrock
-#   end
-#   apos.each do |x,z|
-#     world[x,z,cmin+size+offset-1]=void_block
-#   end
-#   fill = ->(x,z){
-#     air_flag = true
-#     (amin..cmin+size+offset).reverse_each do |y|
-#       id = world[x,z,y]&.id
-#       air_flag = false if block_ids.include?(id) || id == MCWorld::Block::StillWater.id
-#       world[x,z,y] = air_flag ? void_block : MCWorld::Block::Bedrock
-#     end
-#   }
-#   (-offset...size+offset).each do |i|
-#     fill.call ox+i, oz-offset
-#     fill.call ox+i, oz+size+offset-1
-#     fill.call ox-offset, oz+i
-#     fill.call ox+size+offset-1, oz+i
-#   end
-# end
-# File.write jsonfile, areas.to_json
-# # File.write outfile, world.encode
+
+def gen_chunk arr, world
+  size = 16
+  areas = []
+  chunkinfo = {}
+  (1...31).to_a.repeated_permutation(2).each do |i, j|
+    x0,y0 = 16*i,16*j
+    cheights = 16.times.to_a.repeated_permutation(2).map{|ix, iy|arr[x0+ix][y0+iy]}
+    aheights = (-7...16+7).to_a.repeated_permutation(2).map{|ix, iy|arr[x0+ix][y0+iy]}
+    cmin, cmax = cheights.minmax
+    amin = aheights.min
+    next if cmax<63||cmin<62||cmax-cmin>4
+    chunkinfo[[i,j]] = {x: i*16, z: j*16, cmin: cmin-1, cmax: cmax-1, amin: amin-1}
+  end
+  chunks = chunkinfo.values.select{|c|c[:cmax]>=63&&c[:cmin]>=62&&c[:cmax]-c[:cmin]<=4}.shuffle.sort_by{|a|a[:cmax]-a[:cmin]}
+  occupied = {}
+
+  void_block = MCWorld::Block[217]
+  block_ids = Set.new %w(Bedrock Stone Sandstone Grass Dirt Gravel Sand Sandstone IronOre CoalOre Cobblestone).map{|name|
+    MCWorld::Block.const_get(name).id
+  }
+  chunks.each do |c|
+    x,z,cmin,cmax,amin = c.values
+    next if [-size,0,size].repeated_permutation(2).any?{|i,j|occupied[[x+i,z+j]]}
+    occupied[[x,z]] = true
+    size = 16
+    offset = 8
+    areas << {
+      print: {
+        min: {x: x, z: z, y: cmin},
+        max: {x: x+size-1, z: z+size-1, y: cmin+size-1}
+      },
+      area: {
+        min: {x: x-offset+1, z: z-offset+1, y: amin},
+        max: {x: x+size+offset-2, z: z+size+offset-2, y: cmin+size+offset-1}
+      }
+    }
+    (8-size/2-1...8+size/2+1).to_a.repeated_permutation(2) do |i,j|
+      (amin...cmin).each{|y|world[x+i,z+j,y]=MCWorld::Block::Bedrock}
+    end
+    (-1..size).each do |i|
+      world[x+i, z-1, cmin-1]=MCWorld::Block::Bedrock
+      world[x+i, z+size, cmin-1]=MCWorld::Block::Bedrock
+      world[x-1, z+i, cmin-1]=MCWorld::Block::Bedrock
+      world[x+size, z+i, cmin-1]=MCWorld::Block::Bedrock
+
+      world[x+i, z-1, cmin+size]=MCWorld::Block::Bedrock
+      world[x+i, z+size, cmin+size]=MCWorld::Block::Bedrock
+      world[x-1, z+i, cmin+size]=MCWorld::Block::Bedrock
+      world[x+size, z+i, cmin+size]=MCWorld::Block::Bedrock
+      world[x-1,z-1,cmin+i]=MCWorld::Block::Bedrock
+      world[x-1,z+size,cmin+i]=MCWorld::Block::Bedrock
+      world[x+size,z-1,cmin+i]=MCWorld::Block::Bedrock
+      world[x+size,z+size,cmin+i]=MCWorld::Block::Bedrock
+    end
+    (8-size/2-offset...8+size/2+offset).to_a.repeated_permutation(2) do |i,j|
+      world[x+i,z+j,amin-1]=MCWorld::Block::Bedrock
+      world[x+i,z+j,cmin+size+offset-1]=void_block
+    end
+    fill = ->(x,z){
+      (amin..cmin+size+offset).each do |y|
+        id = world[x,z,y]&.id
+        if block_ids.include?(id) || id == MCWorld::Block::StillWater.id
+          world[x,z,y] = MCWorld::Block::Bedrock
+        else
+          world[x,z,y] = void_block
+        end
+      end
+    }
+    (-offset...size+offset).each do |i|
+      fill.call x+i, z-offset
+      fill.call x+i, z+size+offset-1
+      fill.call x-offset, z+i
+      fill.call x+size+offset-1, z+i
+    end
+  end
+  File.write 'areas.json', areas.to_json
+  File.write "spigot/world/region/r.0.0.mca", world.encode
+end
+binding.pry
+gen_chunk arr, world
