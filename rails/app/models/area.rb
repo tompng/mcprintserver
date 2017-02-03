3f6154b0a823f8e1a602ada824c9e03cb342d520
tompng <tomoyapenguin@gmail.com>
2017-02-03 23:42:44 +0900

    add area

---
+++ rails/app/models/area.rb
@@ -0,0 +1,13 @@
+class Area < ActiveRecord::Base
+  def self.prepare
+    coords = load_data.keys.map { |k| k.split('_') }
+    areas = coords.map do |i, j|
+      Area.where(coord_i: i, coord_j: j).first_or_create
+    end
+    Area.where.not(id: areas).destroy_all
+  end
+
+  def self.load_data
+    JSON.parse Net::HTTP.get URI.parse('http://localhost:4567/user_list')
+  end
+end
